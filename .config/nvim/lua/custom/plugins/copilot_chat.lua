fc8b2342279b42059ced3d474fb90d11e14316c8
Vladimir Jancar <jvladko@gmail.com>
2025-02-24 16:16:14 +0100

    nenviem haha

--- .config/nvim/lua/custom/plugins/copilot_chat.lua
+++ .config/nvim/lua/custom/plugins/copilot_chat.lua
@@ -8,21 +8,34 @@ return {
     },
     build = 'make tiktoken',
     opts = {
-      model = 'o3-mini',
+      model = 'o3-mini:github_models',
       question_header = os.getenv 'USER' .. ' ',
       answer_header = 'Jurko Petras ',
       chat_autocomplete = false,
       window = {
-        layout = 'float', -- 'vertical', 'horizontal', 'float', 'replace'
-        width = 0.75, -- fractional width of parent, or absolute width in columns when > 1
-        height = 0.75, -- fractional height of parent, or absolute height in rows when > 1
+        layout = 'vertical', -- 'vertical', 'horizontal', 'float', 'replace'
+        width = 0.45, -- fractional width of parent, or absolute width in columns when > 1
+        height = 1, -- fractional height of parent, or absolute height in rows when > 1
         col = 30,
         row = 1,
       },
       mappings = {
         complete = {
           insert = '<Tab>',
-          normal = '<Tab>',
+        },
+      },
+      prompts = {
+        Gramatika = {
+          prompt = '>$gpt-4o\n\nPrepis text so Slovenskou diakritikou, gramaticke chyby, ciarky a slovosled smies upravit, no slova alebo vyznam viet nemen.',
+          system_prompt = 'You are thorough and precise when getting your task done. You will do exactly what user wants you to. You will be tasked to help with text, do not write line numbers',
+          mapping = '<leader>csk',
+          description = 'Will rewrite slovak text with correct grammar',
+        },
+        GrammarCheck = {
+          prompt = '>$gpt-4o\n\nRewrite the text with correct English grammar, punctuation, and sentence structure. Do not change the meaning of the sentences.',
+          system_prompt = 'You are thorough and precise when getting your task done. You will do exactly what user wants you to. You will be tasked to help with text, do not write line numbers',
+          mapping = '<leader>cen',
+          description = 'Will rewrite English text with correct grammar',
         },
       },
     },
