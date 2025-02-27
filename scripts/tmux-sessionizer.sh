5f3928b0590845d70821ed4bed6ad5c26b658d07
Vladimir Jancar <jvladko@gmail.com>
2025-02-27 15:09:03 +0100

    added missing files to tmux selection

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/dotfiles/ ~/code/ ~/programming_shit/ ~/school/ ~/school/2year2semester/ ~/side_projects/ -mindepth 0 -maxdepth 1 -type d 2>/dev/null | fzf)
+    selected=$(find ~/dotfiles/ ~/code/ ~/programming_shit/ ~/school/ ~/school/2year2semester/ ~/school/2year1semester/ ~/side_projects/ -mindepth 0 -maxdepth 1 -type d 2>/dev/null | fzf)
 fi
 
 if [[ -z $selected ]]; then
