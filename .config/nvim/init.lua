3325504985127f5427e020ddd24e2da5a7e357fd
Vladimir Jancar <jvladko@gmail.com>
2025-03-22 22:18:47 +0100

    transparent background

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -253,18 +253,18 @@ vim.keymap.set({ 'v', 'n' }, 'b', 'B', { noremap = true, silent = true })
 vim.keymap.set({ 'v', 'n' }, 'e', 'E', { noremap = true, silent = true })
 
 -- Make the background transparent
--- vim.cmd [[
---   highlight Normal guibg=none
---   highlight NonText guibg=none
---   highlight NormalNC guibg=none
---   highlight LineNr guibg=none
---   highlight SignColumn guibg=none
---   highlight EndOfBuffer guibg=none
---   highlight VertSplit guibg=none
---   highlight StatusLine guibg=none
---   highlight TabLineFill guibg=none
---   highlight CursorLine guibg=none
--- ]]
+vim.cmd [[
+  highlight Normal guibg=none
+  "highlight NonText guibg=none
+  highlight NormalNC guibg=none
+  "highlight LineNr guibg=none
+  "highlight SignColumn guibg=none
+  "highlight EndOfBuffer guibg=none
+  "highlight VertSplit guibg=none
+  "highlight StatusLine guibg=none
+  "highlight TabLineFill guibg=none
+  highlight CursorLine guibg=none
+]]
 
 -- close buffer with <leader>bd
 vim.keymap.set('n', '<leader>bd', ':bd<CR>', { noremap = true, silent = true })
