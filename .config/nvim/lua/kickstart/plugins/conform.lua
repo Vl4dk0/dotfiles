36664cdb8c89a40eda4e92054815b8ef50057ce2
Vladimir Jancar <jvladko@gmail.com>
2025-04-04 21:00:33 +0200

    ruff will no longer remove unused imports

--- .config/nvim/lua/kickstart/plugins/conform.lua
+++ .config/nvim/lua/kickstart/plugins/conform.lua
@@ -31,7 +31,7 @@ return { -- AUTOFORMAT, FORMATTING, FORMATTERS
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'ruff_fix', 'ruff_format' },
+        python = { 'ruff_format', 'ruff_organize_imports' },
         bash = { 'shfmt' },
         zsh = { 'beautysh' },
         sh = { 'shfmt' },
