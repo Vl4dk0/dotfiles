12189063948d240f4857e042637c35b1483119ec
Vladimir Jancar <jvladko@gmail.com>
2025-06-28 05:24:11 +0200

    alpha v2 :p

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: param-type-mismatch
 return {
   -- The plugin specification for alpha-nvim
   'goolord/alpha-nvim',
@@ -11,18 +12,36 @@ return {
     -- Import the dashboard theme
     local dashboard = require 'alpha.themes.dashboard'
 
-    -- These are the buttons that you can press to perform actions.
-    -- Each button has a key, an icon, a description, and a command to execute.
-    dashboard.section.buttons.val = {
-      dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
-      dashboard.button('f', '  Find file', ':Telescope find_files <CR>'),
-      dashboard.button('g', 'grep  Find word', ':Telescope live_grep <CR>'),
-      dashboard.button('r', '  Recent files', ':Telescope oldfiles <CR>'),
-      dashboard.button('l', '鈴  Lazy', ':Lazy<CR>'),
-      dashboard.button('q', '  Quit', ':qa<CR>'),
-    }
+    -- Functions for the dashboard
+    local function find_files()
+      local builtin = require 'telescope.builtin'
+      builtin.find_files {
+        find_command = {
+          'fd',
+          '--type',
+          'f',
+          '--strip-cwd-prefix',
+          '--hidden',
+          '--no-ignore',
+          '-E',
+          '.git',
+          '-E',
+          'node_modules',
+          '-E',
+          '.next',
+          '-E',
+          '.venv',
+        },
+      }
+    end
+
+    local function live_grep()
+      local builtin = require 'telescope.builtin'
+      builtin.live_grep { additional_args = { '--hidden' } }
+    end
 
     local function header()
+      math.randomseed(os.time())
       local headers = {
         {
           '███╗   ██╗██╗   ██╗██╗██╗   ██╗██╗',
@@ -52,11 +71,21 @@ return {
           '            __/ |            ',
           '           |___/             ',
         },
+        {
+          '   _____           ',
+          '  / ____|          ',
+          ' | |  __  ___  ___ ',
+          ' | | |_ |/ _ \\/ __|',
+          ' | |__| |  __/ (__ ',
+          '  \\_____|\\___|\\___|',
+          '                   ',
+        },
       }
       return headers[math.random(#headers)]
     end
 
     local function footer()
+      math.randomseed(os.time())
       local quotes = {
         'Rakovina by ce pobila.',
         'I use Neovim btw.',
@@ -64,17 +93,23 @@ return {
         'Vedel si o tom ?',
         'Wen TOP rapper.',
         'Vcera mi zhorel doom.',
+        'Najvacsia diera je prievidza',
       }
       return quotes[math.random(#quotes)]
     end
 
-    -- You can create your own ASCII art here: https://patorjk.com/software/taag/
+    -- Dashboard configuration
     dashboard.section.header.val = header()
-
+    dashboard.section.buttons.val = {
+      dashboard.button('e', '  New file', ':ene <BAR> startinsert <CR>'),
+      dashboard.button('f', '  Find file', find_files),
+      dashboard.button('g', 'grep  Find word', live_grep),
+      dashboard.button('r', '  Recent files', ':Telescope oldfiles <CR>'),
+      dashboard.button('l', '鈴  Lazy', ':Lazy<CR>'),
+      dashboard.button('q', '  Quit', ':qa<CR>'),
+    }
     dashboard.section.footer.val = footer()
 
-    -- This table defines the layout of the dashboard.
-    -- It specifies the order and spacing of the sections.
     dashboard.config.layout = {
       { type = 'padding', val = 2 },
       dashboard.section.header,
