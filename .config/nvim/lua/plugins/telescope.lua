b50e69a5454cfcc3b0c1651bf60f22a1bc2ca603
Vladimir Jancar <jvladko@gmail.com>
2025-07-28 03:01:57 +0200

    fix branch

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return { -- SEARCH TOOL
   {
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    branch = 'master',
     dependencies = {
       'nvim-lua/plenary.nvim',
       {
