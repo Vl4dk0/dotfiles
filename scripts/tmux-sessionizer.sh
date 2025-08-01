69c1de585fdbd02c9f2a39931e0f93d55e6c41f4
Vladimir Jancar <jvladko@gmail.com>
2025-08-01 23:25:40 +0200

    update this shit

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/dotfiles/ ~/Documents/ -mindepth 0 -maxdepth 1 -type d 2>/dev/null | fzf)
+    selected=$(find ~/dotfiles/ ~/Documents/ ~/Documents/nabu/Web_scraping_data/legal_ai/scrapping/microservices/ -mindepth 0 -maxdepth 1 -type d 2>/dev/null | fzf)
 fi
 
 if [[ -z $selected ]]; then
