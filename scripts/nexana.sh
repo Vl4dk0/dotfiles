23f77844a6b5733789d33e1c6a9defe329420885
Vladimir Jancar <jvladko@gmail.com>
2025-09-22 00:52:18 +0200

    nexana script

---
+++ scripts/nexana.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ -n "$TMUX" ]]; then
+    echo "Detaching from the current tmux session..."
+    tmux detach-client
+fi
+
+if tmux has-session -t nexana 2>/dev/null; then
+    echo "Session 'nexana' already exists. Attaching..."
+    tmux attach -t nexana
+else
+    tmux new -d -s nexana
+
+    tmux split-window -h -t nexana:1
+    tmux split-window -v -t nexana:1.1
+    tmux split-window -v -t nexana:1.3
+
+    tmux send-keys -t nexana:1.1 "cd ~/Documents/nexana" C-m
+    tmux send-keys -t nexana:1.1 "cls" C-m
+    tmux send-keys -t nexana:1.2 "cd ~/Documents/nexana" C-m
+    tmux send-keys -t nexana:1.2 "cls" C-m
+    tmux send-keys -t nexana:1.3 "cd ~/Documents/nexana" C-m
+    tmux send-keys -t nexana:1.3 "cls" C-m
+    tmux send-keys -t nexana:1.4 "cd ~/Documents/nexana" C-m
+    tmux send-keys -t nexana:1.4 "cls" C-m
+
+    tmux send-keys -t nexana:1.1 "yarn dev" C-m
+    tmux send-keys -t nexana:1.3 "pa reverb:start" C-m
+    tmux send-keys -t nexana:1.4 "pa horizon" C-m
+
+    tmux new-window
+    tmux send-keys -t nexana:2 "cd ~/Documents/nexana" C-m
+    tmux send-keys -t nexana:2 "cls" C-m
+
+    tmux attach -t nexana
+fi
