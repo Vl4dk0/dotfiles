4b51c7ac0fd9ff2510675ee4f8cd77300ca6f416
Vladimir Jancar <jvladko@gmail.com>
2025-09-27 03:05:23 +0200

    move autofunction

--- .config/nvim/lua/functions.lua
+++ .config/nvim/lua/functions.lua
@@ -130,4 +130,65 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc)
+      vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+    end
+
+    -- defaults:
+    -- https://neovim.io/doc/user/news-0.11.html#_defaults
+
+    --  To jump back, press <C-t>.
+    map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+    map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+    map('gtd', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype [D]efinition')
+    map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+    map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    map('<leader>ld', vim.diagnostic.open_float, '[L]sp [D]iagnostics')
+    map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+    map('<leader>h', vim.lsp.buf.hover, 'Show hover information')
+    map('gl', vim.diagnostic.open_float, 'Open Diagnostic Float')
+    map('<leader>v', '<cmd>vsplit | lua vim.lsp.buf.definition()<cr>', 'Goto Definition in Vertical Split')
+
+    -- local function client_supports_method(client, method, bufnr)
+    --   if vim.fn.has 'nvim-0.11' == 1 then
+    --     return client:supports_method(method, bufnr)
+    --   else
+    --     return client.supports_method(method, { bufnr = bufnr })
+    --   end
+    -- end
+
+    -- local client = vim.lsp.get_client_by_id(event.data.client_id)
+    -- if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+    --   local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+    --
+    --   -- When cursor stops moving: Highlights all instances of the symbol under the cursor
+    --   -- When cursor moves: Clears the highlighting
+    --   vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+    --     buffer = event.buf,
+    --     group = highlight_augroup,
+    --     callback = vim.lsp.buf.document_highlight,
+    --   })
+    --   vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+    --     buffer = event.buf,
+    --     group = highlight_augroup,
+    --     callback = vim.lsp.buf.clear_references,
+    --   })
+    --
+    --   -- When LSP detaches: Clears the highlighting
+    --   vim.api.nvim_create_autocmd('LspDetach', {
+    --     group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+    --     callback = function(event2)
+    --       vim.lsp.buf.clear_references()
+    --       vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+    --     end,
+    --   })
+    -- end
+  end,
+})
+
 return fun
