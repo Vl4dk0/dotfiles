93d13cc67d42fe38d5aa6fd9c90b0cca7bc0d427
Vladimir Jancar <jvladko@gmail.com>
2025-11-06 17:26:20 +0100

    fix paths

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/dotfiles/ ~/Documents/ ~/School/ ~/Documents/nabu/Web_scraping_data/ ~/CSES/ -mindepth 0 -maxdepth 1 -type d 2>/dev/null | fzf)
+    selected=$(find ~/dotfiles/ ~/Documents/ ~/School/ ~/nexana-all/ ~/CSES/ -mindepth 0 -maxdepth 1 -type d 2>/dev/null | fzf)
 fi
 
 if [[ -z $selected ]]; then
